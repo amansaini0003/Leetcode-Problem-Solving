e1bcb5573017af946bbeb864a85f7e677702de9c
Aman Saini <48579930+amansaini0003@users.noreply.github.com>
2022-05-20 19:31:19 +0530

    Time: 14 ms (6.52%), Space: 7.9 MB (13.17%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,38 +1,33 @@
 class Solution {
-private: 
-    int traverse(int i, int j, vector<vector<int>> &t ,vector<vector<int>> &obstacleGrid) {
-       
-        cout << i << " " << j << endl; 
-        if(i>= 0 && j>=0 && obstacleGrid[i][j] == 1)
-            return 0;
-        
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        
-        if(i < 0 || j < 0){
-            return 0;
-        }
-            
-        if(t[i][j] != -1)
-            return t[i][j];
-    
-        int left = traverse(i, j-1, t, obstacleGrid);
-        int up = traverse(i-1, j, t, obstacleGrid);
-
-        return t[i][j] = left+up;
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int i = 0, j = 0;
         int row = obstacleGrid.size() , col = obstacleGrid[0].size();
-        vector<vector<int>> t(row+1, vector<int> (col+1, -1));
+        vector<vector<int>> t(row, vector<int> (col));
         
-        if(obstacleGrid[row-1][col-1] == 1 || obstacleGrid[0][0] == 1){
-            return 0;
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                 if(obstacleGrid[i][j] == 1){
+                    t[i][j] = 0;
+                }
+                else if(i==0 && j==0){
+                    t[i][j] = 1;
+                }
+                else {
+                    int up = 0, left = 0;
+                    
+                    if(i>0){ 
+                        up = t[i-1][j]; 
+                    }
+                    if(j>0){ 
+                        left = t[i][j-1]; 
+                    }
+                    
+                    t[i][j] = up + left;
+                }
+            }
         }
         
-        return traverse(row-1, col-1, t, obstacleGrid);
-        
+        return t[row-1][col-1];
     }
 };
