fe0a69f968b52c5ad4e64f1528807553ec0848c0
Aman Saini <48579930+amansaini0003@users.noreply.github.com>
2022-06-08 23:35:38 +0530

    Time: 2 ms (43.62%), Space: 6.1 MB (97.70%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int j = s.size()-1;
+        
+        for(int i=0; i<j; i++){
+            if(s[i] != s[j])
+                return 2;
+            j--;
+        }
+        return 1;
+    }
+};
