4c205b9158b61791cdbd574f28d705906a00d2bc
Aman Saini <amansaini0003@gmail.com>
2022-07-15 13:59:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,18 +1,17 @@
 class Solution {
-private: 
-    int t[46];
-    int countWaysToReachTop(int idx , int n) {
-        if(idx >= n)
-            return 1;
-        if(t[idx] != -1)
-            return t[idx];
-        return t[idx] = countWaysToReachTop(idx+1, n) + countWaysToReachTop(idx+2, n);
-    }
-    
 public:
     int climbStairs(int n) {
-        memset(t, -1, sizeof(t));
-        int ways = countWaysToReachTop(1, n) ;
-        return ways;
+       if(n < 2)
+           return 1;
+        
+        int a = 1;
+        int b = 1;
+        int c;
+        for(int i=2; i<=n; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 };
